17662eb02c1eaa9ca03133e23dc127c0a314b937
seel88 <s202270080@kfupm.edu.sa>
2026-04-06 08:35:17 +0300

    Fix TODO 3: Use double quotes for import

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,4 +1,4 @@
-// Import random.js file
+import { getRandomInt } from "./utils/random.js";
 
 const quotes = [
   "Code is like humor. When you have to explain it, it’s bad.",
@@ -8,7 +8,6 @@ const quotes = [
 ];
 
 // TODO 3: Create and export getRandomQuote function
-//Hint: export function getRandomQuote(){}
 export function getRandomQuote() {
   const index = getRandomInt(quotes.length);
   return quotes[index];
