00c87cd574f5fa1d4fb285a6cf7da5ef619887aa
seel88 <s202270080@kfupm.edu.sa>
2026-04-06 08:47:18 +0300

    Done TODO 6: Implement root and API routes

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -28,9 +28,18 @@ app.listen(PORT, () => {
 });
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote API!");
+});
 
 // TODO 6.2: Create "/api/quote" route
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote: quote });
+});
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 // TODO 7: Start server using app.listen
